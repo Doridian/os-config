8a5204227b7d927218cf0e18514083e833e329e0
Doridian <git@doridian.net>
2021-11-04 21:53:03 -0700

    ensure scaling off

--- align-screens.sh
+++ align-screens.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-displayplacer "id:472826ED-0B86-4AEA-A36B-6AA4AA8B6685 res:3840x1600 hz:120 color_depth:7 origin:(0,0) degree:0" "id:0FD1CB06-A293-4205-B496-DDF558589006 res:1920x1080 hz:60 color_depth:8 scaling:off origin:(-1920,185) degree:0" "id:6C4A7B6D-F984-4DA6-93FD-E0CFA236B0B6 res:1920x1080 hz:60 color_depth:8 scaling:off origin:(3840,203) degree:0"
+displayplacer "id:472826ED-0B86-4AEA-A36B-6AA4AA8B6685 res:3840x1600 hz:120 color_depth:7 scaling:off origin:(0,0) degree:0" "id:0FD1CB06-A293-4205-B496-DDF558589006 res:1920x1080 hz:60 color_depth:8 scaling:off origin:(-1920,185) degree:0" "id:6C4A7B6D-F984-4DA6-93FD-E0CFA236B0B6 res:1920x1080 hz:60 color_depth:8 scaling:off origin:(3840,203) degree:0"
