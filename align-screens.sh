086fe20003faaeac07e3d97646a84d20633c07ab
Doridian <git@doridian.net>
2021-11-19 14:06:06 -0800

    fix

--- align-screens.sh
+++ align-screens.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-displayplacer "id:472826ED-0B86-4AEA-A36B-6AA4AA8B6685 res:3840x1600 hz:120 color_depth:7 scaling:off origin:(0,0) degree:0" "id:0FD1CB06-A293-4205-B496-DDF558589006 res:1920x1080 hz:60 color_depth:8 scaling:off origin:(-1920,185) degree:0"
+displayplacer "id:472826ED-0B86-4AEA-A36B-6AA4AA8B6685 res:3840x1600 hz:120 color_depth:7 scaling:off origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1728x1117 hz:120 color_depth:8 scaling:on origin:(3840,0) degree:0" "id:0FD1CB06-A293-4205-B496-DDF558589006 res:1920x1080 hz:60 color_depth:8 scaling:off origin:(-1920,0) degree:0"
