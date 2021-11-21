c3b08d297f7360c6d8a2a943e3d9b888aea102ef
Doridian <git@doridian.net>
2021-11-21 09:32:20 -0800

    hdmi 2.1

--- align-screens.sh
+++ align-screens.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-displayplacer "id:472826ED-0B86-4AEA-A36B-6AA4AA8B6685 res:3840x1600 hz:120 color_depth:7 scaling:off origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1728x1117 hz:120 color_depth:8 scaling:on origin:(3840,0) degree:0" "id:28A8970F-408E-4B77-BF78-72C32BC0EDBF res:2560x1440 hz:120 color_depth:8 scaling:off origin:(-2560,0) degree:0"
+displayplacer "id:472826ED-0B86-4AEA-A36B-6AA4AA8B6685 res:3840x1600 hz:120 color_depth:7 scaling:off origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1728x1117 hz:120 color_depth:8 scaling:on origin:(3840,0) degree:0" "id:D3A5942F-AD10-4945-AA11-6AE26A59EE65 res:2560x1440 hz:120 color_depth:7 scaling:off origin:(-2560,0) degree:0"
